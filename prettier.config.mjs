2d50e60402ae248c9d85992fb8d61c1f13187e4d
Zeyad Alagamy <wezza2003@gmail.com>
2025-03-12 06:28:26 +0200

    project init

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  semi: true,
+  experimentalTernaries: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  singleAttributePerLine: true,
+  proseWrap: "preserve",
+  insertPragma: false,
+  printWidth: 100,
+  requirePragma: false,
+  tabWidth: 2,
+  useTabs: false,
+  embeddedLanguageFormatting: "auto",
+};
+
+export default config;
