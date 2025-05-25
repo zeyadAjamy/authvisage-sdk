a32aaff398289a3d80210ee11d4430f103d832cd
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-25 04:26:47 +0300

    feat: enhance package configuration and update exports
    - Updated package.json to include module, types, and exports fields for better compatibility with ESM and TypeScript.
    - Added build and development scripts using tsup for building the project in both CommonJS and ESM formats.
    - Included new dependencies for tsup and updated existing dependencies.
    - Refactored src/index.ts to export additional types and classes, improving the SDK's usability.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { AuthVisageClient } from "@/auth/authVisageClient";
+export { TokenManager } from "@/auth/tokenManager";
 export type { User, TokenResponse } from "@/types";
+export type { ClientOptions } from "@/schemas/clientOptions";
+export type { Callback } from "@/auth/tokenManager";
