ee46f3bc7f2f56f82222bc5a77edf21c29454473
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-25 05:37:16 +0300

    fix: update authvisage-sdk version and adjust API endpoint URLs

--- example/app/lib/authvisage.ts
+++ example/app/lib/authvisage.ts
@@ -2,9 +2,9 @@ import { AuthVisageClient, type User } from "authvisage-sdk";
 
 const client = new AuthVisageClient({
   projectId: "03296b21-5185-49d3-9d9e-dd2caa717f67",
-  backendUrl: "http://localhost:9000/api/v1/",
+  backendUrl: "http://localhost:9000/api/v1",
   platformUrl: "http://localhost:3000",
-  redirectUrl: "http://localhost:5173/",
+  redirectUrl: "http://localhost:5173",
 });
 
 export const signInWithFaceAuth = async () => {
