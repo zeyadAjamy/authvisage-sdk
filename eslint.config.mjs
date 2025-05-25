719f29c529a4146797110c3d1dd3a16af8348094
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-25 10:08:44 +0300

    test: update ESLint configuration to include rules for unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,13 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+    },
+  }
 );
